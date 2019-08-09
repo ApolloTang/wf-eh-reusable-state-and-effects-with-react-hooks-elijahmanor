a1c1b384fb2a3960d47a7de89754a6501894dbf0
Apollo Tang <tang.apollo@gmail.com>
2019-08-09 02:56:06 -0400

    08 refactor reducerHook with localstorage

--- wf-webpack-03-babel-eslint/08-01-useReducer-todo-only/src/App.js
+++ wf-webpack-03-babel-eslint/08-01-useReducer-todo-only/src/App.js
@@ -1,4 +1,4 @@
 import React from "react";
-import TodoList from "./TodoList.reducer3";
+import TodoList from "./TodoList.reducer3b-refactor";
 
 export default () => <TodoList />;
--- wf-webpack-03-babel-eslint/08-01-useReducer-todo-only/src/TodoList.reducer3b-refactor.js
+++ wf-webpack-03-babel-eslint/08-01-useReducer-todo-only/src/TodoList.reducer3b-refactor.js
@@ -1,9 +1,9 @@
 import React, { useState, useRef, useReducer } from "react";
+import { useEffect, useMemo } from "react";
 import NewTodo from "./NewTodo";
 import TodoItem from "./TodoItem";
 import { Container, List } from "./Styled";
 import hookReducerLogger from './hookReducerLogger'
-
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -22,16 +22,37 @@ const reducer = (state, action) => {
 }
 
 
+const useReducerWithLocalStorage = (key, defaultData) => {
+  const readLocalStorage = (_key, _defaultData) => JSON.parse(
+    window.localStorage.getItem(_key) || JSON.stringify(_defaultData)
+  )
+  const writeLocalStorage = (_key, _data) => {
+    window.localStorage.setItem(_key, JSON.stringify(_data))
+  }
+
+  const [data, dispatch] = useReducer(
+    hookReducerLogger(reducer),
+    useMemo(()=>readLocalStorage(key, defaultData), [])
+  );
+  useEffect(()=>{ writeLocalStorage(key, data) }, [data]);
+
+  return [data, dispatch];
+};
+
+
+const getNextId = _todos => _todos.reduce((acc,todo)=>Math.max(acc, todo.id), 0)
+
+
 export default function TodoList() {
   const [newTodo, updateNewTodo] = useState("");
   const handleNewChange = e => updateNewTodo(e.target.value);
 
-  const [todos, dispatch] = useReducer(hookReducerLogger(reducer), [/*initial state*/]);
-  const todoId = useRef(0)
+  const [todos, dispatch] = useReducerWithLocalStorage('todos', []);
+  const nextId = useRef(getNextId(todos))
 
   const handleNewSubmit = e => {
     e.preventDefault();
-    dispatch({ type: "ADD_TODO", payload:{id: ++todoId.current, text: newTodo} });
+    dispatch({ type: "ADD_TODO", payload:{id:++nextId.current, text: newTodo} });
     updateNewTodo("");
   };
   const handleDelete = (id) => { dispatch({ type: "DELETE_TODO", payload:{id}  }); };
